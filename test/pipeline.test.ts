ddf11ee16f7d06f36ae52b18388cd810bb8067ce
kumar <kumar@email.com>
2023-01-27 17:47:10 +0530

    Fix Pipeline Unittest

--- test/pipeline.test.ts
+++ test/pipeline.test.ts
@@ -1,17 +1,16 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Pipeline from '../lib/pipeline-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as Pipeline from '../lib/pipeline-stack';
+import {expectProp} from "aws-cdk-lib/pipelines/lib/private/javascript";
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/pipeline-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Pipeline.PipelineStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test('Pipeline Stack', () => {
+  const app = new cdk.App();
+    // WHEN
+  const stack = new Pipeline.PipelineStack(app, 'MyTestStack');
+    // THEN
+  const template = Template.fromStack(stack);
+  expect(template.toJSON()).toMatchSnapshot();
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
 });
